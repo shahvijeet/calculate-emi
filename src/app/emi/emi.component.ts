fc551d7c78c0cf2b9a09b8a16cb2cbbf3e16fc23
shahvijeet <vijeetshah1408@gmail.com>
2023-03-16 10:45:11 +0530

    Emi-calc

--- src/app/emi/emi.component.ts
+++ src/app/emi/emi.component.ts
@@ -12,6 +12,7 @@ public r:number = 10.0;
 public emi:number = 0;
 public totalinterest:number = 0;
 public totalpayment:number = 0;
+public intrests:number = 0;
 public payments: any[] = [];
 
 public amountchange(e:any){
@@ -28,7 +29,7 @@ calculate(){
   this.emi = this.p * this.r * Math.pow(1 + this.r, this.n) / (Math.pow(1 + this.r, this.n) - 1);
   this.totalinterest = this.emi * this.n - this.p;
   this.totalpayment = this.emi * this.n;
- 
+ this.intrests =(this.totalinterest/this.totalpayment)*100;
    this.generatePayments(this.emi, this.r);
       return (this.emi);
 }
